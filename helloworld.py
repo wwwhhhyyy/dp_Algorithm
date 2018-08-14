3edabb8f7aaf4ad22c901c1028a261ca428d08bc
llt <longtaoleng093@foxmail.com>
2018-08-14 10:43:41 -0700

    finished the 0-1 and complete knapsack problems with 2 methods.

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 
 # DP
@@ -37,28 +38,104 @@ def rec_subset(arr, i, s):
     else:
         return rec_subset(arr, i-1, s-arr[i]) or rec_subset(arr, i-1, s)
 
-W = 10
-values = [6,3,5,4,6]
-weights = [2,2,6,5,4]
+def dp_subset(arr, S):
+    opt_arr = np.zeros((len(arr), S+1), dtype=np.bool)
+    opt_arr[:, 0] = True
+    
+    if arr[0] < S:
+        opt_arr[0, arr[0]] = True
+    
+    for i in range(1, len(arr)):
+        for s in range(1, S+1):
+            if arr[i] > s:
+                opt_arr[i, s] = opt_arr[i-1, s]
+            else:
+                opt_arr[i, s] = opt_arr[i-1, s] or opt_arr[i-1, s-arr[i]]
+    x, y = opt_arr.shape
+    return opt_arr[x-1, y-1]
 
 # 0-1 Knapsack problem 
-def rec_knapsack(warr, varr, i, W):
+def rec_01_knapsack(warr, varr, i, W):
     if i == 0 and warr[0] > W:
         return 0
     if i == 0 and warr[0] <= W:
         return varr[0]
     if warr[i] > W:
-        return rec_knapsack(warr, varr, i-1, W)
+        return rec_01_knapsack(warr, varr, i-1, W)
 
     return max(
-        rec_knapsack(warr, varr, i-1, W),
-        rec_knapsack(warr, varr, i-1, W-warr[i]) + varr[i]
+        rec_01_knapsack(warr, varr, i-1, W),
+        rec_01_knapsack(warr, varr, i-1, W-warr[i]) + varr[i]
     )
 
+def dp_01_knapsack(warr, varr, W):
+    opt_arr = np.zeros((len(warr), W+1), dtype=np.int32)
+    
+    opt_arr[0, warr[0]:] = varr[0]
+    opt_arr[:, 0] = 0
+
+    for i in range(1, len(warr)):
+        for w in range(W+1):
+            if warr[i] > w:
+                opt_arr[i, w] = opt_arr[i-1, w]
+            else:
+                opt_arr[i, w] = max(
+                    opt_arr[i-1, w],
+                    opt_arr[i-1, w-warr[i]] + varr[i]
+                )
+    
+    x, y = opt_arr.shape
+    return opt_arr[x-1, y-1]
 
 
-max_value = rec_knapsack(weights, values, len(weights)-1, W)
-print(max_value)
+def rec_complete_knapsack(warr, varr, i, W):
+    if i == 0 and warr[0] > W:
+        return 0
+    if i == 0 and warr[0] <= W:
+        return rec_complete_knapsack(warr, varr, i, W-warr[i]) + varr[i]
+    
+    if warr[i] > W:
+        return rec_complete_knapsack(warr, varr, i-1, W)
+    else:
+        return max(
+            rec_complete_knapsack(warr, varr, i-1, W),
+            rec_complete_knapsack(warr, varr, i, W-warr[i]) + varr[i]
+        )
+
+def dp_complete_knapsack(warr, varr, W):
+    opt_arr = np.zeros((len(warr), W+1), dtype=np.int32)
+    opt_arr[:, 0] = 0
+    for i in range(W+1):
+        opt_arr[0, i] = varr[0] * (i // warr[0])
+    
+    for i in range(1, len(warr)):
+        for w in range(1, W+1):
+            if warr[i] > w:
+                opt_arr[i, w] = opt_arr[i-1, w]
+            else:
+                opt_arr[i, w] = max(
+                    opt_arr[i-1, w],
+                    opt_arr[i, w-warr[i]] + varr[i]
+                )
+    x, y = opt_arr.shape
+    return opt_arr[x-1, y-1]
+
+if __name__ == "__main__":
+
+    values = [6,3,5,4,6]
+    weights = [3,2,6,5,4]
+
+    for t_w in range(30):
+        value1 = rec_01_knapsack(weights, values, len(weights)-1, t_w)
+        value2 = dp_01_knapsack(weights, values, t_w)
+        print("rec: %d | dp: %d"%(value1, value2))
+
+    for t_w in range(30):
+        value1 = rec_complete_knapsack(weights, values, len(weights)-1, t_w)
+        value2 = dp_complete_knapsack(weights, values, t_w)
+        
+        print("w: %d: rec: %d | dp: %d"%(t_w, value1, value2))
+
 
 
 
